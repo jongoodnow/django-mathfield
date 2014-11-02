4473683284fc50feb858dbfd0b572105d5c90876
Jon <goodnow.jonathan@gmail.com>
2014-11-02 00:10:33 -0400

    Max_length kwarg accounted for. Inheriting the form field from CharField

--- mathfield/models.py
+++ mathfield/models.py
@@ -1,17 +1,31 @@
 from django.db import models
+from mathfield.widgets import MathFieldWidget, MathFieldForm
 import json
 
 class MathField(models.TextField):
 
-    description = "A field that allows you to write LaTeX and display it as HTML."
+    description = "Field that allows you to write LaTeX and display it as HTML."
 
     def to_python(self, value):
-        """ The data gets stored into a single text field. The html appears first
-            and is contained with in a span with the class 'katex'. The rest of
-            the string is the raw input from the user."
+        """ The data gets stored into a single text field. The html appears 
+            first and is contained with in a span with the class 'katex'. The 
+            rest of the string is the raw input from the user.
         """
+        if not value:
+            return None
         json_dec = json.decoded.JSONDecoder()
         return json_dec.decode(value)
 
     def get_prep_value(self, value):
         return json.dumps(value)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': MathFieldForm, 
+            'help_text': 'Type text as you would normally. If you want to write LaTeX, surround it with $ characters.',
+            'widget': MathFieldWidget,
+        }
+        defaults.update(kwargs)
+        field = super(MathField, self).formfield(**defaults)
+        field.max_length = self.max_length
+        return field
--- mathfield/tests/models.py
+++ mathfield/tests/models.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from mathfield.models import MathField
 
 class Math(models.Model):
-    name = models.CharField(max_length=200)
     math =  MathField()
 
 admin.site.register(Math)
